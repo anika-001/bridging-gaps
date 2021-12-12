28521b737c5a803fc23c5a1b793964c489a5feb6
krithika2411 <krithisarva7@gmail.com>
2021-12-12 18:28:34 +0530

    ts

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -4,6 +4,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { logindata, regdata } from '../JSONdata/signin';
 import { AuthService } from '../services/auth.service';
+import { WindowService } from '../services/window.service';
+// import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
 
 @Component({
   selector: 'app-signin',
@@ -17,10 +22,30 @@ errormessage:any;
   
   login: boolean = true;
   error: any;
-
+  windowRef: any;
   selectedrole: any;
   signindata: any;
   signupdata: any;
+  phoneno: boolean = false;
+  signindataphoneno: any;
+  signindataemail: any;
+  
+  // firebase.initilizeApp(this.config);
+  // this.windowRef = this.win.windowRef;
+
+  phone() {
+    this.phoneno = !this.phoneno;
+    if (this.phoneno == true) {
+      this.signindata = this.signindataphoneno;
+      // document.querySelector("#recaptcha-container").style.display = "none";
+    }
+
+    else {
+      // this.windowRef.recaptchaVerifier.clear();
+      this.signindata = this.signindataemail;
+    }
+  }
+
 
   move() {
     this.login = !this.login;
@@ -55,8 +80,12 @@ errormessage:any;
   })
   
   ngOnInit(): void {
+    // firebase.initializeApp(this.config);
+    
     this.signindata = logindata;
     this.signupdata = regdata;
+    // this.signindataphoneno= logindataphoneno;
+
   }
 
 }
