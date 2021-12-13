3b00aa89b5f21f629b94085f4cc5058d2f6c9417
krithika2411 <krithisarva7@gmail.com>
2021-12-14 02:13:46 +0530

    Merge https://github.com/ieeebridginggaps/bridginggaps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultationComponent } from './consultation/consultation.component';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 
 const routes: Routes = [
   {
+    path: 'consultation',
+    component: ConsultationComponent,
+  },
+  {
     path: 'signin',
-    component: SigninComponent
+    component: SigninComponent,
   },
   {
     path: '',
