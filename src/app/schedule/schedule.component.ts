fde6d2b7858afcbbed1f92eaf3e349c4ac1e2a76
krithika2411 <krithisarva7@gmail.com>
2021-12-25 16:51:37 +0530

    weekly calender

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ScheduleComponent implements OnInit {
 
   constructor() { }
-
+  time: Array<number> = [];
+  days = ["MON", "TUE", "WED", "THUR", "FRI", "SAT", "SUN"];
   ngOnInit(): void {
+    for (let i = 0; i <48; i++)
+     {
+       this.time.push(i);
+     }
   }
-
+  
 }
