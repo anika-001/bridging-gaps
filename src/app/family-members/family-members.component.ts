f5fb82ae76552dcee154e3372759c7757d3b2deb
Preeti3024 <preetikpati13@gmail.com>
2021-12-27 16:57:55 +0530

    t

--- src/app/family-members/family-members.component.ts
+++ src/app/family-members/family-members.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { DatabaseopService } from '../services/databaseop.service';
 
 @Component({
   selector: 'app-family-members',
@@ -9,6 +10,9 @@ import { AuthService } from '../services/auth.service';
 })
 export class FamilyMembersComponent implements OnInit {
   user:any;
+  members: any;
+  currentfamilymember: any = 0;
+
   test: any = {
     'relation': 'grandmama',
     'name': 'Ajji',
@@ -24,13 +28,26 @@ export class FamilyMembersComponent implements OnInit {
 
   testArrayKeys: Array<'relation' | 'name' | 'age' | 'medhistory' | 'triggers' | 'dietplan' | 'gender' | 'phone' | 'currenthelp' | 'helphistory'> = ['name', 'age', 'relation', 'gender', 'phone', 'currenthelp', 'medhistory', 'dietplan', 'helphistory', 'triggers'];
   testArrayFields: Array<String> = ['Name', 'Age', 'Relation', 'Gender', 'Phone Number', 'Help', 'Medical History', 'Diet Plan', 'Help History', 'Reminders']
-  constructor(private as: AuthService, private router: Router) {}
+  constructor(private as: AuthService, private router: Router, private db: DatabaseopService) {}
 
   ngOnInit(): void {
     this.as.getUserState().subscribe(res => {
       if (!res) this.router.navigate(['/signin'])
       this.user = res;
+      this.getfamilymembers();
     })
   }
 
+  fam() {
+    this.router.navigate(['/form'], { queryParams: { id: 1 } });
+  }
+  // diet() {
+  //   this.router.navigate(['/form'], { queryParams: { id: 5, fmid: this.currentfamilymember } });
+  // }
+  getfamilymembers() {
+    this.db.readCollection(`familymembers/${this.user.uid}/familymember`).snapshotChanges().subscribe(res => {
+      this.members = res;
+
+    })
+  }
 }
