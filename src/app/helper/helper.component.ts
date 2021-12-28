7f01f9d79a3de795ca3509cbc777530a1d6b92b4
krithika2411 <krithisarva7@gmail.com>
2021-12-28 17:04:44 +0530

    helper

---
+++ src/app/helper/helper.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { DatabaseopService } from '../services/databaseop.service';
+
+@Component({
+  selector: 'app-helper',
+  templateUrl: './helper.component.html',
+  styleUrls: ['./helper.component.scss']
+})
+export class HelperComponent implements OnInit {
+  user: any;
+  caretaker: any;
+  currenthelper: any = 0;
+  currenthelperid: any;
+
+  testArrayKeys: Array<'HelperName' | 'HelperAge' | 'Gender' | 'HelperLocation' | 'HelperPhoneNumber' | 'HelperOrganisation' |'HelperDescription' > = ['HelperName', 'HelperAge','Gender', 'HelperLocation', 'HelperPhoneNumber','HelperOrganisation',  'HelperDescription'];
+  testArrayFields: Array<String> = ['Name', 'Age', 'Gender', 'Location','Phone Number', 'Organization', 'Description']
+  constructor(private as: AuthService, private router: Router, private db: DatabaseopService) { }
+
+  ngOnInit(): void {
+    this.as.getUserState().subscribe(res => {
+      if (!res) this.router.navigate(['/signin'])
+      this.user = res;
+      this.gethelper();
+    })
+
+  }
+  
+  helperclick(helper: any, currenthelperid: any) {
+    this.currenthelper = helper;
+    this.currenthelperid = currenthelperid;
+    //  console.log(this.currentfamilymember);
+
+  }
+
+
+  gethelper() {
+    this.db.readCollection(`Caretakers`).snapshotChanges().subscribe(res => {
+      this.caretaker= res; 
+      this.currenthelperid = res[0].payload.doc.id;
+    })
+ 
+  }
+
+}
